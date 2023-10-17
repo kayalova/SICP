e7feeb5ea1b2d1930b3aab3b847b7da2023fcc93
Zarema <zarema.klva@gmail.com>
2023-10-17 07:16:50 +0300

    Add description

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # SICP
-My solutions to SICP exercises
+
+My solutions to [SICP](https://web.mit.edu/6.001/6.037/sicp.pdf) exercises
+
+> Structure and Interpretation of Computer Programs by Harold Abelson and Gerald Jay Sussman with Julie Sussman
+
+![](https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1391032527i/43713.jpg)
+
+## To install MIT Scheme on macOS with M1 chip
+[MIT Scheme on Apple Silicon (M1 Mac) - Kenneth Friedman](https://kennethfriedman.org/thoughts/2021/mit-scheme-on-apple-silicon/)
+
+## To run
+```zsh
+#file
+scheme --quiet < main.scm
+
+#repl
+scheme
+```
---
+++ main.scm
@@ -0,0 +1,60 @@
+; scheme --quiet < main.scm
+; предикаты
+(define (≥ x y)
+	(or (> x y) (= x y)))
+
+(define z 6)
+(display (and (> z 5) (< z 10)))(newline)
+;----------------------------------------
+
+; условные выражения
+(define (abs3 x)
+  (if (< x 0)
+      (- x)
+      x)
+)
+
+(define (abs2 x)
+  (cond ((< x 0) (- x))
+        (else x)
+  )
+)
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))
+  )
+)
+
+(display (abs3 -5))(newline)
+;----------------------------------------
+
+; определение процедуры
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(display (sum-of-squares 3 4))(newline)
+
+(define (square x) (* x x))
+(display (square 5))(newline)
+(display (+ (square 12) (square 13)))(newline)
+(define q 5)
+(define w 2)
+(display (+ (square q) (square w)))(newline)
+;-------------------------------------------
+
+(define a (* (+ 2 (* 4 6))
+             (+ 3 5 7))
+); 26 * 15 = 450;
+(display a)(newline)
+
+(define size 2)
+(display size)(newline)
+
+; чтобы облегчить чтение
+(display (+ (* 3
+               (+ (* 2 4)
+                  (+ 3 5)))
+            (+ (- 10 7)
+                6))
+)(newline); 9 + 48 = 57
