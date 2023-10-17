d74629606d30327777b748f4263f470cac1f6dd9
Zarema <zarema.klva@gmail.com>
2023-10-17 12:56:30 +0300

    Solve ex1.03

--- chapter_1/ex1.03.scm
+++ chapter_1/ex1.03.scm
@@ -2,29 +2,7 @@
 ; Define procedure that takes three numbers as arguments and returns
 ; the sum of the squares of the two larger numbers
 
-;
-(display (define x (sum-of-squares-of-two-largest-among-three 1, 2, 3)))(newline)
-
-(define (sum-of-squares-of-two-largest-among-three a b c) (+ (square (find-largest-of-three a b c))
-                                                             (square (find-second-largest a b c))))
-
-(define (square x) (* x x))
-
-(define (find-largest-of-three a b c)
-        (cond ((and (> a b) (> a c)) a)
-               ((and (> b a) (> b c)) b)
-               (else c) ))
-
-; errors
-(define (find-second-largest a b c)
-          (define largest-of-three (find-largest-of-three a b c)
-          (cond ((= largest-of-three a) (find-largest-of-two b c))
-                ((= largest-of-three b) (find-largest-of-two a c))
-                (else (find-largest-of-two a b)))))
-
-(display (define x (find-second-largest 1 2 3)))(newline)
-
-(define (find-largest-of-two a b) (if (> a b) a b))
-
-; на вход даны 3 числа - найти первое наибольшее +
-; на вход даны 3 числа - найти второе наибольшее
+(define (sum-larger-square a b c)
+            (cond ((and (< a b) (< a c)) (+ (* b b) (* c c)))
+                  ((and (< b a) (< b c)) (+ (* a a) (* c c)))
+                  (else (+ (* b b) (* a a)))))
